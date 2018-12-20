36ecd874f383db5517be44d6e440439b3494763f
Caitlin Sigler <41393772+caitlinsigler@users.noreply.github.com>
2018-12-20 14:13:26 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  homework 6
+//
+//  Created by Caitlin Sigler on 11/3/16.
+//  Copyright © 2016 Caitlin Sigler. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+string find_value(int A[], int size, int value){
+    bool isFound= 0;
+    string index= "";
+    for (int i=0; i<size; i++){
+        if (A[i]== value){
+            index+= to_string(i);
+            index+= " ";
+            isFound= 1;
+        }
+    }
+    if (isFound==0){
+        cout<< "value not found";
+    }
+    return index;
+}
+
+int main() {
+   
+    int size=7;
+    int value=88;
+    int A[]= {11, 2, 15, 88, 99, 11, 88};
+    
+    cout<< find_value(A, size, value)<< endl;
+    
+    return 0;
+}
