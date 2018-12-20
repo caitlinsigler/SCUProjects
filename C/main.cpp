a0b8456385cd38e6dc98a4c3bc0cc08f6d7a5cbb
Caitlin Sigler <41393772+caitlinsigler@users.noreply.github.com>
2018-12-20 15:32:24 -0800

    Add files via upload

---
+++ C/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  8.2
+//
+//  Created by Caitlin Sigler on 11/17/16.
+//  Copyright © 2016 Caitlin Sigler. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Point2D {
+    double x;
+    double y;
+};
+
+void printPoint(Point2D p){
+    cout<< "("<<p.x<<","<< p.y<< ")\n";
+}
+
+Point2D getPoint(int x, int y) {
+    Point2D point;
+    point.x = x;
+    point.y = y;
+    return point;
+}
+double distance(Point2D p, Point2D q){
+    return sqrt(pow(p.x-q.x,2)+pow(p.y-q.y, 2));
+}
+
+int main() {
+    
+    int x= 1;
+    int y= 2;
+    int x1= 3;
+    int y1= 4;
+    Point2D p= getPoint(x,y);
+    Point2D q= getPoint(x1,y1);
+    
+    cout<< distance(p, q)<<endl;
+    printPoint(p);
+    
+    return 0;
+}
