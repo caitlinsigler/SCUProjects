bf1560c9c23c9fc0a2448fa5805f814436dbf2dc
Caitlin Sigler <41393772+caitlinsigler@users.noreply.github.com>
2018-12-20 16:06:04 -0800

    Update matrixMath.cpp

--- C/matrixMath.cpp
+++ C/matrixMath.cpp
@@ -56,7 +56,7 @@ Matrix matrixAdd(Matrix m1, Matrix m2) {
     }
     return m;
 }
-Matrix matrixSum (Matrix mats[],int size){
+Matrix matrixSum (Matrix matrices[],int size){
     Matrix s;
     for (int i=0; i<size; i++) {
         s= matrixAdd(s, mats[i]);
